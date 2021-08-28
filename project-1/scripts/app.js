e9bc5b0df91cc7300dcef13b77197f1c5422024e
Cosmas Cieplak <cosc@Cosmass-iMac.local>
2021-08-28 22:39:46 +0100

    super deep into this search and destroy function, after this -> drag and drop and  minor ai tweaks and styling are left

--- project-1/scripts/app.js
+++ project-1/scripts/app.js
@@ -76,9 +76,7 @@ function init() {
       lessNumbers.push(i-1)
     }
   }
-
   let backUpNumbers = loadsOfNumbers.filter(num => !lessNumbers.includes(num))
-
 
   // INITIALIZE BUTTONS AS DISABLED UNTIL GAME STARTED
   rotateB.disabled = true
@@ -190,7 +188,6 @@ function init() {
   let currentShipIndex = 0
   let selectedShip = destroyerP
 
-
   // ~~~~~   GRID CONSTRUCTION AND UTILIZATION START ~~~~~~
   const grid = document.querySelector('.grid')
   const gridP = document.querySelector('.gridP')
@@ -223,9 +220,8 @@ function init() {
 
 
 // ~~~~~   GAME PLAY SECTION    ~~~~~~
-  
-  // GRID BASED CONSTANTS
 
+  // GRID BASED CONSTANTS
   const computerBoxes = document.querySelectorAll('.grid div')
 
   // FUNCTIONS
@@ -434,9 +430,12 @@ function init() {
 
   // AI levels of guessing loaded upon difficulty level
   ////    i. random guessing
-  //    ii. advanced guessing
-  //    iii. search and destroy after a hit
-  //        a) hunting true/false variable b) hit location stored c) to shoot at squares loaded d) end hunting if ship destroyed
+  ////    ii. advanced guessing
+  // iii. search and destroy after a hit
+  ////    a) hunting true/false variable 
+  //    b) hit location stored 
+  //    c) to shoot at squares loaded 
+  //    d) end hunting if ship destroyed
   //    iV. re-evaluate andvanced guessing after destorying smallest ships / clear central limited area before chasing outer ships
 
   // *** need to change player and computer guessing to change targets class to hit or miss ***
@@ -447,12 +446,49 @@ function init() {
   // SEARCH AND DESTROY --- THE CRUX FOR MVP AND ESSENTIALLY FOR MEDIUM AND HARD DIFFICULTIES
   //        a) hunting true/false variable b) hit location stored c) to shoot at squares loaded d) end hunting if ship destroyed
   // I need global variables outside all the functions so they can be updated and then reference again in a new loop through
+  
+  // SEARCH AND DESTROY GLOBAL CONSTANTS --- FEED INTO SEARCH AND DESTROY AND INTO COMPUTER GUESS
   let hunting = false
-  let guessHitLocation = '' //this will be a string (gridP + double digit number)
+  let hitLocation = ''
+  let huntingGuess = ''
+  const huntingLocations = []
 
-  function searchAndDestroy(){
+  function searchAndDestroy(hunting, guessStatus){
     // need a function that tells the ai what its next guess will be
+    // if hunting is true get your guess from search and destroy, otherwise get a new random guess
+    console.log(hunting)
+    console.log(!hunting)
+    console.log(guessStatus)
+    if (hunting is false && guessStatus === 'hit'){ // exclamation point hunting
+    } else if (hunting is true && guessStatus === 'hit'){
+
+    } else if (hunting is false && guessStatus === 'miss') {
+
+    } else if (hunting is true && guessStatus === 'miss') {
+
+    } else if (hunting is false && guessStatus === 'clear') {}
+
     
+    
+    console.log('The last hunting guess was a ' + guessStatus)
+
+    // if not hunting, and hit is passed in INITIALIZE, and go counter clockwise
+    // if hunting miss REVOLVE
+    // if hunting and hit, restart and go counter clockwise
+    // if hunting and hit, double down (hard part)
+    // if hunting, and hit again, 
+
+    if(guessStatus === 'hit'){
+
+    } else{
+
+    }
+    
+    hitLocation -> feed 4 new locations into huntingLocations[] 
+    then iterate through them based on a counter each time the function is running
+    
+    huntingGuess = results of this function's work
+    //must reassign a new value to huntingGuess
   }
 
   function computerGuess(){
@@ -462,48 +498,85 @@ function init() {
     let warning = ''
     
     // If the computer runs out of squares using the efficient 'advanced' guessing method, it will resort to guessing the rest of the squares
+    // this may end up being completely unecessary
     if (lessNumbers.length === 0){
-      console.log('computer is attacking remaining squares')
       lessNumbers = backUpNumbers
-    } else {
-      console.log(`${lessNumbers.length} numbers left`)
     }
+
+    // I don't need this code for validation at the moment:
+    // console.log('computer is attacking remaining squares')
+    // else {
+    //   console.log(`${lessNumbers.length} numbers left`)
+    // }
 
     // ~~~~~~~~   MODULAR GUESS INFORMATION HERE   ~~~~~~~~
     //easy guess
     let numGuessEasy = doubleDigits(loadsOfNumbers[getRandomInt(loadsOfNumbers.length)])
     let indexGuessEasy = loadsOfNumbers.indexOf(Number(numGuessEasy))
-    loadsOfNumbers.splice(indexGuessEasy, 1)
     let compGuessLEasy = 'gridP' + numGuessEasy
-
     //advanced INITIAL guess
     let numGuessAdv = doubleDigits(lessNumbers[getRandomInt(lessNumbers.length)])
     let indexGuessAdv = lessNumbers.indexOf(Number(numGuessAdv))
-    lessNumbers.splice(indexGuessAdv, 1)
     let compGuessLAdv = 'gridP' + numGuessAdv
+    // hunting guess
+    //re-assigned above by the search and destroy function
+    // huntingGuess
+    let indexHunting = lessNumbers.indexOf(Number(huntingGuess))
+    let huntingId = 'gridP' + doubleDigits(huntingGuess)
 
     // SELECTION OF INITIAL GUESS BASED ON DIFFICULTY
-    if (difficultyLevel === 'easy' || difficultyLevel === 'medium'){
-      console.log('computer is taking an easy / medium guess')
-      guessLoc = document.getElementById(compGuessLEasy)
-      guessClass = guessLoc.className
-    } else {
-        console.log('computer is taking a hard guess')
+    if (difficultyLevel === 'easy' || difficultyLevel === 'medium'){ // easy guess tree
+      if (hunting){ // if hunting is true, the last guess was a hit, and we will hunt until we destroy the ship hit
+        lessNumbers.splice(indexHunting, 1)
+        guessLoc = document.getElementById(huntingId)
+        guessClass = guessLoc.className
+        console.log('running search and destroy easy')
+      } else { // procure a new easy guess and eliminate it from the remaining guesses
+        loadsOfNumbers.splice(indexGuessEasy, 1)
+        guessLoc = document.getElementById(compGuessLEasy)
+        guessClass = guessLoc.className
+      }
+    } else { // advancing guessing tree
+      if (hunting){ // if hunting is true, the last guess was a hit, and we will hunt until we destroy the ship hit
+        lessNumbers.splice(indexHunting, 1)
+        guessLoc = document.getElementById(huntingId)
+        guessClass = guessLoc.className
+        console.log('running search and destroy advanced')
+      } else { // procure a new advanced guess and eliminate it from the remaining guesses
+        lessNumbers.splice(indexGuessAdv, 1)
         guessLoc = document.getElementById(compGuessLAdv)
         guessClass = guessLoc.className
+      }
+
     }
 
-    // PROCESSING THE GUESS SECTION
+    // PROCESSING THE GUESS SECTION (AS A HIT OR MISS OR A HIT THAT DESTROYS A SHIP)
 
     if (guessLoc.style.backgroundColor != 'red' && playerClasses.includes(guessClass) ){
         let damagedShip = playersShips[playerClasses.indexOf(guessClass)]
         damagedShip.damage++
         scoreP -= 100
         guessLoc.style.backgroundColor = 'red'
+        if(difficultyLevel === 'medium' || difficultyLevel === 'hard'){
+          if (hunting === false){
+            console.log(guessLoc.id, 'should be the id of the guessed square')
+            hitLocation = guessLoc.id
+            console.log(hitLocation, 'should be the the same id stored in the hitLocation variable')
+            console.log('hunting triggered!')
+            hunting = true
+          }
+        
+          searchAndDestroy(hunting, 'hit')
+        }
+
         if(damagedShip.damage === damagedShip.hitPoints){
           console.log(`They destroyed our ${guessClass.slice(0,-1)} Admiral!`)
           scoreP -= 100
           damagedShip.destroyed = true
+          //when a ship is destroyed: these changes allow the appropriate level of difficulty random guessing to occur again
+          hunting = false // the hunting flag should be turned off
+          hitLocation = '' //the hit location (original hit) should be wiped
+          searchAndDestroy(hunting, 'clear') // and this may not be necessary, but should clear out the search and destroy for a new target
           destroyedShipsP.push(guessClass)
           if (destroyedShipsP.length === 5){
             console.log("They have destroyed our fleet! The day is lost! Your score: "+ scoreP)
@@ -517,20 +590,10 @@ function init() {
         }
     } else if (guessLoc.style.backgroundColor === ''){
       console.log('They missed us Admiral!')
+      searchAndDestroy('miss')
       guessLoc.style.backgroundColor = 'grey'
     }
   }
-
-  // function parseDifficulty(){
-  //   if (difficultyLevel === 'hard'){
-  //     console.log('this does not exist yet')
-  //   } else if (difficultyLevel === 'medium') {
-  //     console.log('this does not exist yet')
-  //   } else {
-  //     console.log('computer easy guess')
-  //     computerGuessEasy()
-  //   }
-  // }
 
   // if (confirm("Confirm Firing Coordinates. Choose wisely, they will return fire.")){}
 
